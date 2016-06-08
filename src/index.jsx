916728981955f01bfd173fe4c447f2322ec98699
Scott Morley <scott.morley@insidesales.com>
2016-06-07 21:49:47 -0600

    Quill shadow DOM issue POC

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import  ReactDOM from 'react-dom';
+
 import {render} from 'react-dom';
 
-import MyEditor from './MyEditor.jsx';
+import Quill from 'quill';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    var element = ReactDOM.findDOMNode(this.refs.editor);
+    var editor = new Quill(element);
+  }
+
   render () {
     return (
       <div>
-        <MyEditor />
+        <div id="editor">
+          <div ref="editor">Hello World!</div>
+        </div>
         <p> Hello React!</p>
       </div>
     );
